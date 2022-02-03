8a7b0ca18fe72e6451b5a45d0b5be2b2917dc018
Lucy Stumpf <2574266s@student.gla.ac.uk>
2022-02-03 11:14:35 +0000

    chapter 3 commit

---
+++ rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# each view takes at least one HttpResponse object and returns one
+def index(request):
+    return HttpResponse('<a href="/rango/about/">About</a>Rango says hey there partner!')
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Index</a>Rango says here is the about page.")
