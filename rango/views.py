7c20f37a46dc8b884e44234b27f9295aee9d8d3c
Lucy Stumpf <2574266s@student.gla.ac.uk>
2022-02-03 12:15:54 +0000

    first chapter 4 commit (p.62)

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 # each view takes at least one HttpResponse object and returns one
 def index(request):
-    return HttpResponse('<a href="/rango/about/">About</a>Rango says hey there partner!')
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/index.html", context=context_dict)
 
 def about(request):
     return HttpResponse("<a href='/rango/'>Index</a>Rango says here is the about page.")
