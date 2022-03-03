5614e2c08af5cda54227d5dcb22e62555d1a7564
2574266s <2574266s@student.gla.ac.uk>
2022-03-03 18:30:30 +0000

    Basic setup

--- Imagenest/urls.py
+++ Imagenest/urls.py
@@ -1,4 +1,4 @@
-"""tango_with_django_project URL Configuration
+"""Imagenest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -22,6 +20,6 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')), # maps any URLs starting with rango/ to rango
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
